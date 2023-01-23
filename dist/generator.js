772f8e1db2316c61567e3aa926f9fc66e5690ad7
Yecine Megdiche <yecine.megdiche@predium.de>
2023-01-23 12:30:41 +0100

    Another release

---
+++ dist/generator.js
@@ -0,0 +1,55 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const generator_helper_1 = require("@prisma/generator-helper");
+const fs = __importStar(require("fs"));
+const internals_1 = require("@prisma/internals");
+const path_1 = __importDefault(require("path"));
+const constants_1 = require("./constants");
+const generatePrismaFaker_1 = require("./helpers/generatePrismaFaker");
+const { version } = require('../package.json');
+(0, generator_helper_1.generatorHandler)({
+    onManifest() {
+        internals_1.logger.info(`${constants_1.GENERATOR_NAME}:Registered`);
+        return {
+            version,
+            defaultOutput: '../generated',
+            prettyName: constants_1.GENERATOR_NAME,
+        };
+    },
+    onGenerate: async (options) => {
+        var _a;
+        const faker = (0, generatePrismaFaker_1.generatePrismaFaker)(options.dmmf.datamodel.models);
+        const writeLocation = path_1.default.join((_a = options.generator.output) === null || _a === void 0 ? void 0 : _a.value, 'faker.ts');
+        await fs.promises.mkdir(path_1.default.dirname(writeLocation), {
+            recursive: true,
+        });
+        await fs.promises.writeFile(writeLocation, faker);
+    },
+});
+//# sourceMappingURL=generator.js.map
---
+++ dist/helpers/generatePrismaFaker.js
@@ -0,0 +1,90 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generatePrismaFaker = void 0;
+class Model {
+    constructor(dmmf_model) {
+        this.name = dmmf_model.name;
+        this.scalars = dmmf_model.fields.filter((field) => field.kind === 'scalar');
+        this.foreignKeys = dmmf_model.fields.flatMap((field) => field.relationFromFields || []);
+        this.scalarsRequiredForCreation = this.scalars.filter((field) => field.isRequired && !field.hasDefaultValue && !this.foreignKeys.includes(field.name));
+        this.objectsRequiredForCreation = dmmf_model.fields.filter((field) => field.kind === 'object' && !field.isList && field.isRequired);
+    }
+}
+class PrismaFakerGenerator {
+    constructor(models) {
+        this.scalarMap = {
+            Int: 'faker.datatype.number()',
+            BigInt: 'faker.datatype.bigInt()',
+            Float: 'faker.datatype.float()',
+            String: 'faker.datatype.string()',
+            DateTime: 'faker.datatype.datetime()',
+            Decimal: 'new Decimal(faker.datatype.number())',
+            Boolean: 'faker.datatype.boolean()',
+            Json: 'null',
+        };
+        this.header = `import { faker } from '@faker-js/faker';
+import * as prisma from '@prisma/client';
+import { Prisma } from '@prisma/client';
+import { Decimal } from '@prisma/client/runtime';
+
+export default class prisma_faker {`;
+        this.footer = `}`;
+        this.models = models.map((dmmfModel) => new Model(dmmfModel));
+    }
+    generate() {
+        return [this.header, this.generateBody(), this.footer].join('\n\n');
+    }
+    generateBody() {
+        return `${this.generateDatabaseFaker()}
+
+${this.generateTypescriptFaker()}`;
+    }
+    generateScalarFaker(field) {
+        if (field.isList) {
+            return '[]';
+        }
+        return `${this.scalarMap[field.type]}`;
+    }
+    generateScalarFieldFaker(field) {
+        return `    ${field.name}: ${this.generateScalarFaker(field)},`;
+    }
+    generateTypescriptFaker() {
+        const typescriptModelFakers = this.models.map((model) => this.generateTypescriptModelFaker(model)).join('\n\n');
+        const fakersExport = this.models.map((model) => `    ${model.name}: prisma_faker.${model.name}_faker,`);
+        const typescriptFakersCollection = `
+  static readonly typescript = {
+${fakersExport.join('\n')}
+  };`;
+        return [typescriptModelFakers, typescriptFakersCollection].join(`\n`);
+    }
+    generateTypescriptModelFaker(model) {
+        const signature = `  static readonly ${model.name}_faker = (partial_${model.name}?: Partial<prisma.${model.name}>): prisma.${model.name} => ({`;
+        const fields = model.scalars.map((field) => this.generateScalarFieldFaker(field));
+        const footer = `    ...partial_${model.name},
+  });`;
+        return [signature, ...fields, footer].join('\n');
+    }
+    generateDatabaseFaker() {
+        const dbModelFakers = this.models.map((model) => this.generateDatabaseModelFaker(model)).join(`\n\n`);
+        const fakersExport = this.models.map((model) => `    ${model.name}: prisma_faker.${model.name}_faker_db,`);
+        const dbFakersCollection = `
+  static readonly db = {
+${fakersExport.join('\n')}
+  };`;
+        return [dbModelFakers, dbFakersCollection].join(`\n`);
+    }
+    generateDatabaseModelFaker(model) {
+        const signature = `  static readonly ${model.name}_faker_db = (partial_${model.name}?: Partial<Prisma.${model.name}CreateInput>): Prisma.${model.name}CreateInput => ({`;
+        const requiredScalars = model.scalarsRequiredForCreation.map((field) => this.generateScalarFieldFaker(field));
+        const requiredObjects = model.objectsRequiredForCreation.map((field) => `    ${field.name}: { create: prisma_faker.${field.type}_faker_db()},`);
+        const footer = `    ...partial_${model.name},
+  });`;
+        return [signature, ...requiredObjects, ...requiredScalars, footer].join(`\n`);
+    }
+}
+function generatePrismaFaker(models) {
+    const generator = new PrismaFakerGenerator(models);
+    return generator.generate();
+}
+exports.generatePrismaFaker = generatePrismaFaker;
+//# sourceMappingURL=generatePrismaFaker.js.map
