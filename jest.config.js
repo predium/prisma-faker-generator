5fdca6c163dbbc010325463418d696875a3d0890
Yecine Megdiche <yecine.megdiche@predium.de>
2024-11-26 13:05:50 +0100

    chore: update packages

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   modulePathIgnorePatterns: ['__helpers__/', '__fixtures__/'],
-}
+  prettierPath: require.resolve('prettier-2'),
+};
